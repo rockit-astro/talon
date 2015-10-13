87de39d925da2d0c1470ddb70fc458a0022c61cf
Paul Chote <p.chote@warwick.ac.uk>
2015-10-13 17:06:55 +0100

    Clean up unused build files.

--- src/utils/xephem/tools/xephemdbd/startxdbd.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-$dbdir = '/usr/local/xephem/catalogs';
-$lkfile = 'xephemdbd.pid';
-$infifo = 'xephemdbd.in';
-$logfn = "xephemdbd.log";
-system "mkfifo $infifo" if (! -e $infifo);
-system "./xephemdbd_static -vtlic 0 $lkfile $infifo $dbdir >> $logfn 2>&1";
