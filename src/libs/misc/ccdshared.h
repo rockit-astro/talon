53de3d24ebfd86a36ebfeef2d4e9cd4072a834e1
Paul Chote <p.chote@warwick.ac.uk>
2015-10-13 16:45:27 +0100

    Strip remaining unused camera support.

--- src/libs/misc/ccdshared.h
+++
@@ -1,3 +0,0 @@
-/* ccdshared.c */
-void parseTemperatureString(char *msg, CCDTempInfo *tp);
-int parseCCDSize(char *msg, CCDExpoParams *cep);
--- src/sdk/fli/libfli-RBI-prerelease/jlibfli/build.bat
+++
@@ -1,7 +0,0 @@
-copy jlibfli\Release\jlibfli.dll
-javac JLibFLI.java
-javac JLibFLITest.java
-
-javah -jni JLibFLI
-copy JLibFLI.h jlibfli
-
