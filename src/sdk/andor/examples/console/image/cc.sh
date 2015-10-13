2ecee46d699e5c2766f65ca8a0921dde85645be4
Paul Chote <p.chote@warwick.ac.uk>
2015-10-13 15:44:42 +0100

    Remove disabled files.

--- src/sdk/andor/examples/console/image/cc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -o image image.cpp -landor -L../../../lib/ -m32
--- src/utils/xcamera/patchlevel.h
+++
@@ -1,5 +0,0 @@
-extern char PATCHLEVEL[];
-
-/* For RCS Only -- Do Not Edit
- * @(#) $RCSfile: patchlevel.h,v $ $Date: 2001/04/19 21:12:00 $ $Revision: 1.1.1.1 $ $Name:  $
- */
