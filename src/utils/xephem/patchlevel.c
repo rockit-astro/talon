4dfab10c5b5a17c9955bdeb48b0978b547ef96bc
Paul Chote <p.chote@warwick.ac.uk>
2015-12-01 11:37:10 +0000

    Remove xephem.

--- src/utils/xephem/patchlevel.c
+++
@@ -1,5 +0,0 @@
-char PATCHLEVEL[] = "Pre-3.4.1";
-char PATCHDATE[] = " 5 Jan 2001";
-
-/* For RCS Only -- Do Not Edit */
-static char *rcsid[2] = {(char *)rcsid, "@(#) $RCSfile: patchlevel.c,v $ $Date: 2001/04/19 21:12:02 $ $Revision: 1.1.1.1 $ $Name:  $"};
